417eb4e47b4b83c5269918f5bea45300fcd9e6c3
JulianskyMe <juliakamynskaya@gmail.com>
2026-07-01 14:05:18 +0300

    flake8 fix

--- cinema/models.py
+++ cinema/models.py
@@ -29,7 +29,7 @@ class Actor(models.Model):
 
     def __str__(self):
         return self.first_name + " " + self.last_name
-    
+
     @property
     def full_name(self):
         return self.first_name + " " + self.last_name
